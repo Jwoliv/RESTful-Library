50eba93b7be7abff7c7531e8b20b79d4aa3c8f79
Lishchuk Bohdan <bogdanlishchuk04@gmail.com>
2023-03-13 19:43:09 +0200

    Initial commit

---
+++ src/main/java/com/example/RESTful/Library/ResTfulLibraryApplication.java
@@ -0,0 +1,13 @@
+package com.example.RESTful.Library;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResTfulLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResTfulLibraryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/RESTful/Library/ResTfulLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.RESTful.Library;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResTfulLibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
